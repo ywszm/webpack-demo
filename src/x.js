24c95897e27d5e7448b33a682f6aad3afebdd795
ywszm <aichengwan1@163.com>
2020-08-07 16:25:59 +0800

    update

--- src/x.js
+++ src/x.js
@@ -1,1 +1,4 @@
+import css from './x.css'
+
+console.log(css);
 export default 'xxx'
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js'
-    }
+        filename: '[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '老丁',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
